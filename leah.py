6fb00102296e76cb078d36def20bfda305690764
xIceArcher <xicearcher@gmail.com>
2019-09-13 01:30:52 +0900

    Add datetime to logger format

--- leah.py
+++ leah.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 
 from utils.credentials_utils import get_credentials
 
-logging.basicConfig(level=logging.INFO)
+formatter = '%(name)s : %(asctime)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=formatter)
 
 active_extensions = ['cogs.PostTweetMedia', 'cogs.PostQuotedTweet', 'cogs.TwitterStalker']
 
