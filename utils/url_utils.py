f27d3596f5dd4f72a398645600734f402ea02139
xIceArcher <xicearcher@gmail.com>
2019-09-19 17:15:56 +0900

    Update instagram regex to include dash and underscore

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -19,5 +19,5 @@ def get_photo_url(url: str, size='orig'):
 
 
 def get_insta_links(s: str):
-    regex = re.compile(r'http[s]?://(?:w{3})?\.instagram\.com/p/[A-Za-z0-9]*/?(?:\?[^ \r\n]*)?')
+    regex = re.compile(r'http[s]?://(?:w{3})?\.instagram\.com/p/[A-Za-z0-9\-_]*/?(?:\?[^ \r\n]*)?')
     return list(OrderedDict.fromkeys(regex.findall(s)))
