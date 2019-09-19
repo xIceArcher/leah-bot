a815a2eb59443c47bb097e1dc496af2e1dbbc234
xIceArcher <xicearcher@gmail.com>
2019-09-19 17:13:14 +0900

    Add logging statements

--- cogs/PostInstaMedia.py
+++ cogs/PostInstaMedia.py
@@ -1,9 +1,13 @@
+import logging
+
 from discord.ext import commands
 
 from utils.discord_embed_utils import get_photo_embed
 from utils.discord_utils import clean_message
 from utils.instagram_utils import get_insta_photo_urls
 from utils.url_utils import get_insta_links
+
+logger = logging.getLogger(__name__)
 
 
 class PostInstaMedia(commands.Cog):
@@ -18,8 +22,12 @@ class PostInstaMedia(commands.Cog):
         cleaned_message = clean_message(message.content)
 
         for link in get_insta_links(cleaned_message):
-            for photo in get_insta_photo_urls(link):
+            photos = get_insta_photo_urls(link)
+
+            for photo in photos:
                 await message.channel.send(embed=get_photo_embed(photo))
+
+            logger.info(f'Instagram URL: {link}, Photos: {photos}')
 
         await self.bot.process_commands(message)
 
