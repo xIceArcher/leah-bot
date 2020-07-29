a65d3f5138fe6d980d669870c5184848b179e15a
xIceArcher <xicearcher@gmail.com>
2020-07-29 15:22:27 +0800

    Add force embed of Twitter posts

--- cogs/PostTweetMedia.py
+++ cogs/PostTweetMedia.py
@@ -2,6 +2,7 @@ import logging
 
 from discord.ext import commands
 
+from utils.discord_embed_twitter_utils import get_tweet_embeds
 from utils.twitter_utils import extract_photo_urls, extract_video_url, get_tweet
 from utils.url_utils import get_tweet_ids
 
@@ -13,6 +14,21 @@ MAX_MEDIA_COUNT = 5
 class PostTweetMedia(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def embed(self, ctx, twitter_url: str):
+        tweet_ids = get_tweet_ids(twitter_url)
+
+        if len(tweet_ids) == 0:
+            await ctx.channel.send("Message does not contain a Twitter link!")
+            return
+
+        tweet = get_tweet(tweet_ids[0])
+
+        for embed in get_tweet_embeds(tweet):
+            await ctx.channel.send(embed=embed)
+
+        logger.info(f'Twitter URL: {twitter_url}, Tweet ID: {tweet_ids[0]}')
 
     @commands.command()
     async def photos(self, ctx, twitter_url: str):
