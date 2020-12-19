1472b19a59877c7962d9309f6531fa3f04af5b0a
xIceArcher <xicearcher@gmail.com>
2020-12-19 20:07:22 +0800

    Ignore invalid commands

--- leah.py
+++ leah.py
@@ -10,16 +10,16 @@ logging.basicConfig(level=logging.INFO, format=formatter)
 active_extensions = ['cogs.Admin', 'cogs.PostQuotedTweet', 'cogs.PostTweetMedia', 'cogs.TwitterStalker', 'cogs.PostTime',
                      'cogs.PostInstaMedia', 'cogs.PostAmebloMedia', 'cogs.TwitterIconStalker', 'cogs.PostYoutubeInfo', 'cogs.InstaStalker']
 
+bot = commands.Bot(command_prefix='!!', description='placeholder')
 
-def main():
-    bot = commands.Bot(command_prefix='!!', description='placeholder')
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    raise error
 
-    for extension in active_extensions:
-        bot.load_extension(extension)
+for extension in active_extensions:
+    bot.load_extension(extension)
 
-    credentials = get_credentials('credentials.json')
-    bot.run(credentials['discord']['token'])
-
-
-if __name__ == '__main__':
-    main()
+credentials = get_credentials('credentials.json')
+bot.run(credentials['discord']['token'])
