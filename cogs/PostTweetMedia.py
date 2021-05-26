f275c97d3e11f31a12a54624fb85d06da3cc8aca
xIceArcher <xicearcher@gmail.com>
2021-05-26 14:48:21 +0800

    Move quoted command into PostTweetMedia

--- cogs/PostTweetMedia.py
+++ cogs/PostTweetMedia.py
@@ -1,10 +1,11 @@
 import logging
 
 from discord.ext import commands
+from tweepy import TweepError
 
 from utils.discord_utils import clean_message
 from utils.discord_embed_twitter_utils import get_tweet_embeds
-from utils.twitter_utils import extract_photo_urls, extract_video_url, get_tweet, get_tweet_url
+from utils.twitter_utils import extract_photo_urls, extract_video_url, get_tweet, get_tweet_url, is_quote
 from utils.url_utils import get_tweet_ids
 
 logger = logging.getLogger(__name__)
@@ -69,6 +70,35 @@ class PostTweetMedia(commands.Cog):
         logger.info(f'{get_tweet_url(tweet)} sent to #{ctx.channel.name} in {ctx.guild.name}')
         await ctx.channel.send(video)
 
+    @commands.command()
+    async def quoted(self, ctx, twitter_url: str):
+        tweet_id = get_tweet_ids(twitter_url)[0]
+
+        if not tweet_id:
+            await ctx.channel.send('Not a valid Twitter URL!')
+
+        tweet = None
+
+        try:
+            tweet = get_tweet(tweet_id)
+        except TweepError:
+            await ctx.channel.send(f'Tweet ID {tweet_id} is not valid!')
+
+        if is_quote(tweet):
+            quoted_tweet = get_tweet(tweet.quoted_status.id)
+
+            for embed in get_tweet_embeds(quoted_tweet):
+                await ctx.channel.send(embed=embed)
+
+            video = extract_video_url(tweet.quoted_status)
+
+            if video:
+                await ctx.channel.send(video)
+
+            logger.info(f'{get_tweet_url(tweet)} sent to #{ctx.channel.name} in {ctx.guild.name}')
+        else:
+            await ctx.channel.send(f'This tweet does not quote any other tweet!')
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content.startswith(self.bot.command_prefix):
@@ -79,7 +109,9 @@ class PostTweetMedia(commands.Cog):
         for tweet_id in get_tweet_ids(cleaned_message):
             tweet = get_tweet(int(tweet_id))
             video_url = extract_video_url(tweet)
-            await message.channel.send(video_url)
+
+            if video_url:
+                await message.channel.send(video_url)
 
         await self.bot.process_commands(message)
 
